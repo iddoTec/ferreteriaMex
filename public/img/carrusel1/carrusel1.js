678b652ae135dfcb11853dff51b736d022f13cf7
RobertCoderDev <roberto.smusic.1@gmail.com>
2024-10-01 19:44:31 -0600

    desarrollo de contact, carruseles, hero1 y pagina de about

---
+++ public/img/carrusel1/carrusel1.js
@@ -0,0 +1,50 @@
+export const carrusel1 = [
+    {
+        img: "img/carrusel1/producto1.jpeg",
+        nombre: "Tanque estacionario Cytsa 120 litros",
+        descripcion: "Tanque de 120Lts marca CYTSA está fabricado con acero que cumple con especificaciones ASTM, con pintura anticorrosiva en polvo horneable color blanca aplicado con proceso electrostático espesor de 50 micrómetros, cuenta con válvulas de seguridad, llenado, servicio e indicador de nivel.",
+        precio: "8,200",
+    },
+    {
+        img: "img/carrusel1/producto2.jpeg",
+        nombre: "Cilindro portátil para gas LP, 30kg",
+        descripcion: "Cuerpo de acero con recubrimiento de pintura electrostática, resistente a la corrosión",
+        precio: "2,150",
+    },
+    {
+        img: "img/carrusel1/producto3.jpeg",
+        nombre: "Regulador Para Gas 1 Via Foset",
+        descripcion: "Diseñado para controlar y mantener una presión de gas constante y segura. Fabricado en latón resistente",
+        precio: "145",
+    },
+    {
+        img: "img/carrusel1/producto4.jpg",
+        nombre: "Cilindro portátil para gas LP, 20lb",
+        descripcion: "Cuerpo de acero con recubrimiento de pintura electrostática, resistente a la corrosión, válvula de servicio con conexión de 7/8 Fácil de transportar",
+        precio: "985",
+    },
+    {
+        img: "img/carrusel1/producto5.jpg",
+        nombre: "Tanque estacionario de 300 litros marca Cytsa",
+        descripcion: "Tanque de 300Lts marca CYTSA está fabricado con acero que cumple con especificaciones ASTM, con pintura anticorrosiva en polvo horneable color blanca aplicado con proceso electrostático espesor de 50 micrómetros, cuenta con válvulas de seguridad, llenado, servicio e indicador de nivel.",
+        precio: "10,999",
+    },
+    {
+        img: "img/carrusel1/producto6.jpg",
+        nombre: "Soplete con manguera gas, 7/8",
+        descripcion: "Con una presión de 200 PSI, este soplete es capaz de generar una flama potente y constante, ideal para soldar, calentar o descongelar tuberías.",
+        precio: "465",
+    },
+    {
+        img: "img/carrusel1/producto7.jpg",
+        nombre: "Regulador de gas L.P de 2 vías, Foset",
+        descripcion: "Su diseño incluye una válvula de cambio manual y viene equipado con 2 pigtail con tuerca izquierda, niple terminal y tuerca cónica de latón, lo que facilita su instalación y asegura una conexión segura",
+        precio: "415",
+    },
+    {
+        img: "img/carrusel1/producto8.jpeg",
+        nombre: "Tanque estacionario 180 litros marca Cytsa",
+        descripcion: "Tanque de 180Lts marca CYTSA está fabricado con acero que cumple con especificaciones ASTM, con pintura anticorrosiva en polvo horneable color blanca aplicado con proceso electrostático espesor de 50 micrómetros, cuenta con válvulas de seguridad, llenado, servicio e indicador de nivel.",
+        precio: "9,500",
+    },
+];
