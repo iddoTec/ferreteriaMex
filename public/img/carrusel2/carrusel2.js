3176364b6f703f07ba6b0b59efc9a37f97aa5f9b
RobertCoder <roberto.smusic.1@gmail.com>
2024-10-15 12:59:10 -0600

    creacion del segundo carrusel

---
+++ public/img/carrusel2/carrusel2.js
@@ -0,0 +1,50 @@
+export const carrusel2 = [
+    {
+        img: "img/carrusel2/producto1.jpg",
+        nombre: "Clarificador Natural Para Piscina Pandapura 1 Litro",
+        descripcion: "Devuelve de manera natural la transparencia y claridad al agua de su piscina. Su fórmula concentrada y de origen natural, permite aglutinar: la suciedad, aceites, lociones y desechos orgánicos, de una manera eficiente y ecológica.",
+        precio: "340",
+    },
+    {
+        img: "img/carrusel2/producto2.jpg",
+        nombre: "Escalera Para Piscinas En Acero Inoxidable Panda 3 Peldaños",
+        descripcion: "La solución perfecta para el acceso seguro y cómodo a tu piscina. Este producto, de la reconocida marca PANDA, pertenece a la serie de Accesorios para piscinas y es ideal tanto para uso doméstico como comercial.",
+        precio: "3,750",
+    },
+    {
+        img: "img/carrusel2/producto3.jpg",
+        nombre: "Bomba Centrífuga Para Alberca Aqua Pak Silver 15 1.5hp 115v",
+        descripcion: "garantizará que tu alberca cumpla con las condiciones de higiene adecuadas. El movimiento que ejercerá sobre el agua contenida, facilitará la desinfección y filtrado de suciedad y residuos.",
+        precio: "3,900",
+    },
+    {
+        img: "img/carrusel2/producto4.jpg",
+        nombre: "Cascada Decorativa Led Tipo Natural Trasera Luz Multicolor",
+        descripcion: " PANDA POOL PRODUCTS. Sectores de uso: Hogar, Comercio. Incluye: Conexión en T para cementar. Garantía: 1 año",
+        precio: "6,795",
+    },
+    {
+        img: "img/carrusel2/producto5.jpg",
+        nombre: "Filtro de arena para alberca de 6 vías FPD24T",
+        descripcion: "Ideal para eliminar impurezas de tu piscina. De simple mantenimiento. Material del tanque: plástico. Caudal máximo de 11.3m³/h",
+        precio: "4,900",
+    },
+    {
+        img: "img/carrusel2/producto6.jpg",
+        nombre: "Manguera Flexible 10.5 Metros Para Albercas Piscinas Panda",
+        descripcion: "Marca: PANDA. Serie: Mantenimiento piscinas. Sectores de uso: Hogar, Comercio. Garantía: 1 año",
+        precio: "solo por WhatsApp",
+    },
+    {
+        img: "img/carrusel2/producto7.jpg",
+        nombre: "Zeolita Mineral 22 Kg Para Sistema De Tratamiento De Agua",
+        descripcion: "Es el medio de filtración granular con el mejor desempeño, hecho a base de un mineral con propiedades únicas. Su estabilidad y microporosidad lo hacen el medio de filtración perfecto para prácticamente cualquier aplicación.",
+        precio: "349",
+    },
+    {
+        img: "img/carrusel2/producto8.jpg",
+        nombre: "Bomba De Calor Hidrocontrol 13000 Btu 127v Spa Jacuzzi 11500",
+        descripcion: "Incluye cable de alimentación de 1.5 m con clavija y adaptador para cementar, para cople de 3/4. Garantía 5 años (compresor) / 2 años (mano de obra y refacciones)",
+        precio: "890 USD",
+    },
+];
