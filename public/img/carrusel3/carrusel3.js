aa1a3d8bbd910d7526d711db4a3bd15c19d57ea1
RobertCoder <roberto.smusic.1@gmail.com>
2024-10-15 17:01:55 -0600

    creacion del tercer carrusel

---
+++ public/img/carrusel3/carrusel3.js
@@ -0,0 +1,50 @@
+export const carrusel3 = [
+    {
+        img: "img/carrusel3/producto1.jpg",
+        nombre: "Regadera Cuadrada Abs 8'' Negra Mate Foset 46748 Color Negro",
+        descripcion: "Con forma cuadrada. Producto fabricado en acero inoxidable. Dimensiones: 20cm de ancho, 3cm de alto y 62mm de diámetro.",
+        precio: "485",
+    },
+    {
+        img: "img/carrusel3/producto2.jpg",
+        nombre: "Regadera Cuadrada 8 , Abs, Con Brazo, Satín Aqua Foset 46742",
+        descripcion: "Regadera cuadrada 8, ABS, con brazo, satín, Aqua Foset. Funciona con presión baja y alta",
+        precio: "525",
+    },
+    {
+        img: "img/carrusel3/producto3.jpg",
+        nombre: "Mezcladora Inox 8 Para Fregadero De Pared Foset",
+        descripcion: "Funciona con baja presión. Cuerpo de latón para máxima duración. Manerales metálicos y cuello de acero inoxidable",
+        precio: "1,115",
+    },
+    {
+        img: "img/carrusel3/producto4.jpg",
+        nombre: "Mezcladora Aqua 4'' Tipo Bar Palanca Satin Foset 45622 Color Gris",
+        descripcion: "Está diseñada para adaptarse a cualquier espacio con personalidad y desarrollar con eficacia tareas como el lavado y la limpieza.",
+        precio: "635",
+    },
+    {
+        img: "img/carrusel3/producto5.jpg",
+        nombre: "Mezcladora Lavabo, Manerales Palanca, Cuello Alto, Foset, Acabado Cromado Color Plateado",
+        descripcion: "Acabado: Cromado. Lugar de colocación: lavabo. Control doble comando. Cierre cerámico. Con mango palanca.",
+        precio: "695",
+    },
+    {
+        img: "img/carrusel3/producto6.jpg",
+        nombre: "Mezcladora lavabo, Manerales Palanca, negro, Aqua Foset",
+        descripcion: "Está diseñada para adaptarse a cualquier espacio con personalidad y desarrollar con eficacia tareas como el lavado y la limpieza.",
+        precio: "849",
+    },
+    {
+        img: "img/carrusel3/producto7.jpg",
+        nombre: "Mezcladora fregadero Manerales Palanca Negro Foset",
+        descripcion: "Mezcladora para fregadero, manerales palanca, NEGRO, Aqua. Funciona con baja presión. Cuerpo de latón para máxima duración",
+        precio: "679",
+    },
+    {
+        img: "img/carrusel3/producto8.jpg",
+        nombre: "Juego De 2 Manerales Grandes Regadera Negro Foset AQMR-62M Acabado Mate Color Negro",
+        descripcion: "Está diseñada para adaptarse a cualquier espacio con personalidad y desarrollar con eficacia tareas como el lavado y la limpieza.",
+        precio: "685",
+    },
+];
