aa5ccf6a651e8ebcf533b7f26fa46a5e8669148f
RobertCoder <roberto.smusic.1@gmail.com>
2024-10-16 16:54:59 -0600

    avance en category

---
+++ public/img/carrusel4/carrusel4.js
@@ -0,0 +1,50 @@
+export const carrusel4 = [
+    {
+        img: "img/carrusel4/producto1.jpg",
+        nombre: "Luminario Plafón Redondo 18w 1600lm Volteck Pla-207l Color Blanco",
+        descripcion: "La combinación perfecta entre iluminación y diseño. Esta lámpara le dará vida a tus espacios para que puedas aprovecharlos al máximo.",
+        precio: "139",
+    },
+    {
+        img: "img/carrusel4/producto2.jpg",
+        nombre: "Luminario Plafón Metálico De Led 18w 1100lm Volteck Pla-703l",
+        descripcion: "Esta lámpara le dará vida a tus espacios para que puedas aprovecharlos al máximo. Además, podrás personalizar tus lugares preferidos y darles un detalle distintivo.",
+        precio: "255",
+    },
+    {
+        img: "img/carrusel4/producto3.jpg",
+        nombre: "Lámpara farol led de pared Volteck estructura color negro",
+        descripcion: "Este modelo es apto para focos con tecnología led, que además de contribuir al cuidado del ambiente, duran más tiempo y te permiten ahorrar hasta un 90 % más que las fuentes de luz convencionales.",
+        precio: "235",
+    },
+    {
+        img: "img/carrusel4/producto4.jpg",
+        nombre: "Lámpara de pared con potencia de 100W",
+        descripcion: "Gracias a su resistente material, podrás instalar esta lámpara en espacios exteriores y asegurarte una excelente calidad de iluminación sin riesgos de deterioros.",
+        precio: "255",
+    },
+    {
+        img: "img/carrusel4/producto5.jpg",
+        nombre: "Plafón Cuadrado Moderno 24 W 6500 K Led Blanco Volteck",
+        descripcion: "Esta lámpara le dará vida a tus espacios para que puedas aprovecharlos al máximo. Además, podrás personalizar tus lugares preferidos y darles un detalle distintivo.",
+        precio: "215",
+    },
+    {
+        img: "img/carrusel4/producto6.jpg",
+        nombre: "Luminaria De Pared Decorativa Acero Inoxidable Exterio",
+        descripcion: "La combinación perfecta entre iluminación y diseño. Esta lámpara le dará vida a tus espacios para que puedas aprovecharlos al máximo.",
+        precio: "325",
+    },
+    {
+        img: "img/carrusel4/producto7.jpg",
+        nombre: "Arbotante Led Decorativo Redondo Volteck",
+        descripcion: "Este modelo es apto para focos con tecnología led, que además de contribuir al cuidado del ambiente, duran más tiempo y te permiten ahorrar hasta un 90 % más que las fuentes de luz convencionales.",
+        precio: "429",
+    },
+    {
+        img: "img/carrusel4/producto8.jpg",
+        nombre: "Farol Colgante Negro, Volteck",
+        descripcion: "COLGANTE FAROL, NEGRO, 1XE26. Diseño tradicional, Compatible con lámpara LED o halógena",
+        precio: "535",
+    },
+];
